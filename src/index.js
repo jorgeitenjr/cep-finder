876111643b5ad8fc16d433832ea1f10a2ef1f2ed
Jorge Luis Iten Júnior <jorgeiten@gmail.com>
2017-11-23 14:11:14 -0200

    initial commit

---
+++ src/index.js
@@ -0,0 +1,134 @@
+const request = require('request');
+const cheerio = require('cheerio');
+const isNode = require('detect-node');
+
+const defaultUrl = 'http://www.buscacep.correios.com.br/sistemas/buscacep/resultadoBuscaCepEndereco.cfm';
+const postalCodeServiceUrl = postalCode => `https://viacep.com.br/ws/${postalCode}/json/`;
+
+const search = ".tmptabela tr";
+const subSearch = "td";
+const encoding = 'binary';
+const splitCharCityState = "/";
+const splitCharStreet = "-";
+const pattern = "^\\d{5}-?\\d{3}$";
+const regExp = new RegExp(pattern);
+const cepNotFound = cep => {
+  return {message: `CEP ${cep} não encontrado.`};
+};
+const invalidValid = cep => {
+  return {message: `CEP ${cep} inválido.`};
+};
+const unableToFindCep = cep => {
+  return {message: `Não foi possível buscar o CEP ${cep}`};
+};
+
+const map = {
+  0: 'logradouro',
+  1: 'bairro',
+  2: 'cidade',
+  3: 'cep',
+  4: 'estado',
+  5: 'complemento'
+};
+
+const cepKey = "relaxation";
+
+const formData = {
+  "semelhante": "N",
+  "tipoCEP": "ALL"
+};
+
+const handleHtmlData = data => {
+  const result = {};
+  data(search).children(subSearch).each((key, element) => {
+    const rowValue = element.children[0].data;
+    if (key == 0) {
+      const values = rowValue.split(splitCharStreet);
+      result[map[key]] = values[0].trim();
+      if (values.length > 1) {
+        values.shift();
+        result[map[5]] = values.join(splitCharStreet).trim();
+      }
+    }
+    else if (key == 2) {
+      const values = rowValue.split(splitCharCityState);
+      result[map[key]] = values[0];
+      result[map[4]] = values[1];
+    } else {
+      result[map[key]] = rowValue.trim();
+    }
+
+  });
+  return result;
+};
+
+const isAValidCep = cep => regExp.test(cep);
+
+
+/**
+ * Search for the address of a CEP (postal code)
+ * @param {string} the postal code
+ * @param {string} in browser it will aways use the "webservice" call,
+ *  running on node, by default it will use Correios html page, if "webservice"
+ *  informed it will use the ViaCEP service call.
+ * @return {object} representing the address
+ */
+const getAddress = async (cep, type) =>
+  isAValidCep(cep) ?
+    isNode ?
+      type == "webservice" ?
+        await getAddressFromService(cep)
+        : await getAddressHtml(cep)
+      : await getAddressFromService(cep)
+    : invalidValid(cep);
+
+
+const getAddressHtml = cep => {
+  return new Promise((resolve, reject) => {
+    const cepToFind = {};
+    cepToFind[cepKey] = cep;
+    request.post(
+      {
+        url: defaultUrl,
+        encoding: encoding,
+        form: Object.assign(formData, cepToFind)
+      }, (err, httpResponse, html) => {
+        if (!err && httpResponse.statusCode == 200) {
+          const data = cheerio.load(html);
+          const handled = handleHtmlData(data);
+          if (Object.keys(handled).length === 0) {
+            resolve(cepNotFound(cep));
+          }
+          resolve(handled);
+        } else {
+          console.log(err); //eslint-disable-line no-console
+          reject(unableToFindCep(cep));
+        }
+      });
+  });
+};
+
+const getAddressFromService = cep => {
+  return new Promise((resolve, reject) => {
+    postalCodeServiceUrl
+    request.get({url: postalCodeServiceUrl(cep)}, (err, httpResponse, body) => {
+      const result = JSON.parse(body);
+      if (err) {
+        reject(unableToFindCep(cep));
+      }
+      if (result.erro) {
+        resolve(cepNotFound(cep));
+      }
+      const handled = {};
+      handled[map[0]] = result[map[0]];
+      handled[map[1]] = result[map[1]];
+      handled[map[2]] = result['localidade'];
+      handled[map[3]] = result[map[3]];
+      handled[map[4]] = result['uf'];
+      handled[map[5]] = result[map[5]];
+      resolve(handled);
+    });
+  });
+};
+
+module.exports = getAddress;
---
+++ src/index.test.js
@@ -0,0 +1,59 @@
+'use strict';
+
+var expect = require('chai').expect;
+const cepFinder = require('./index');
+
+describe('CepFinder', function () {
+  this.timeout(5000);
+  it('should return a valid address object', function (done) {
+    const expectedObject = {
+      logradouro: 'Rua 15 de Novembro',
+      bairro: 'Centro',
+      cidade: 'Blumenau',
+      estado: 'SC',
+      cep: '89010-000',
+      complemento: 'até 900 - lado par'
+    };
+
+
+    cepFinder("89010000").then(result => {
+      expect(result).to.deep.equal(expectedObject);
+      done();
+    });
+
+  });
+  it('should return a valid address object using webservice', function (done) {
+    const expectedObject = {
+      logradouro: 'Avenida Paulista',
+      bairro: 'Bela Vista',
+      cidade: 'São Paulo',
+      estado: 'SP',
+      cep: '01311-300',
+      complemento: "de 1867 ao fim - lado ímpar"
+    };
+
+
+    cepFinder("01311300", "webservice").then(result => {
+      expect(result).to.deep.equal(expectedObject);
+      done();
+    });
+
+  });
+  it('should not find CEP', function (done) {
+    const cep = 98312802;
+    const expected = {message: `CEP ${cep} não encontrado.`};
+    cepFinder(cep).then(result => {
+      expect(result.message).to.deep.equal(expected.message);
+      done();
+    });
+  });
+
+  it('should return an invalid CEP', (done) => {
+    const cep = 123456789;
+    const expected = {message: `CEP ${cep} inválido.`};
+    cepFinder(cep).then(result => {
+      expect(result.message).to.deep.equal(expected.message);
+      done();
+    });
+  });
+});
